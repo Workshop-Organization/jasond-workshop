ac4ac0bdd3e6f4ceb4b5cfbafbdfdcc40e25e0a4
HotJayz <131409881+HotJayz@users.noreply.github.com>
2023-10-10 13:43:09 -0500

    Update cards.go

--- router/coprocessor/cards.go
+++ router/coprocessor/cards.go
@@ -6,7 +6,7 @@ var CARD_RE = regexp.MustCompile(`[0-9]{4}[- ]?[0-9]{4,6}[- ]?([0-9]{4,6}[- ]?)?
 
 func validateCard(input string) (string, error) {
 
-	result := CARD_RE.ReplaceAllString(input, "****-****-****-****")
+	result := CARD_RE.ReplaceAllString(input, "****-****-****-" + input[-4:])
 
 	return result, nil
 }
